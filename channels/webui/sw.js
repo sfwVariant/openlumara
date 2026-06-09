6f524cf6b9a58a0909e113947ad974528b04fa65
Rose22 <rosemelody22@gmail.com>
2026-06-09 22:53:48 +0200

    sw.js

--- channels/webui/sw.js
+++ channels/webui/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'openlumara-v4.7.9';
+const CACHE_NAME = 'openlumara-v4.9.0';
 const ASSETS = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
