c811951771da8d31708fb5b44cd687b8c9b819cc
Rose22 <rosemelody22@gmail.com>
2026-06-28 15:57:48 +0200

    [webui] small styling fix in toolcall results modal

--- channels/webui/sw.js
+++ channels/webui/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'openlumara-v7.4.7';
+const CACHE_NAME = 'openlumara-v7.4.8';
 const ASSETS = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
